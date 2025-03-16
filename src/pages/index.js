0e61048566b93cc2171760d91d940e3a4402d5ad
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-03-15 22:18:18 -0400

    Updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,25 @@ import React from "react";
 import Layout from "@theme/Layout";
 import "../css/index.css";
 import ContentBox from "../components/ContentBox";
-import { withBaseUrl } from "@docusaurus/useBaseUrl"; // Import the correct function
+import { addBaseUrl } from "@docusaurus/useBaseUrl"; // Correct import
 
 export default function Home() {
   const totalRiskData = [
     {
       icon: "/img/TotalRisk.png",
-      doc_location: withBaseUrl("/docs/rmc-totalrisk/user-guide"),
+      doc_location: addBaseUrl("/docs/rmc-totalrisk/user-guide"), // Use addBaseUrl
       doc_name: "RMC-TotalRisk User's Guide",
       active: true,
     },
     {
       icon: "/img/TotalRisk.png",
-      doc_location: withBaseUrl("/docs/rmc-totalrisk/applications-guide"),
+      doc_location: addBaseUrl("/docs/rmc-totalrisk/applications-guide"), // Use addBaseUrl
       doc_name: "RMC-TotalRisk Applications Guide",
       active: true,
     },
     {
       icon: "/img/TotalRisk.png",
-      doc_location: withBaseUrl("/docs/rmc-totalrisk/verification-report"),
+      doc_location: addBaseUrl("/docs/rmc-totalrisk/verification-report"), // Use addBaseUrl
       doc_name: "RMC-TotalRisk Verification Report",
       active: true,
     },
@@ -29,7 +29,7 @@ export default function Home() {
   const RFAData = [
     {
       icon: "img/RFA.png",
-      doc_location: withBaseUrl("/docs/rmc-rfa/user-guide"),
+      doc_location: addBaseUrl("/docs/rmc-rfa/user-guide"), // Use addBaseUrl
       doc_name: "RMC-RFA User's Guide",
       active: true,
     },
@@ -38,7 +38,7 @@ export default function Home() {
   const bestFitData = [
     {
       icon: "img/BestFit.png",
-      doc_location: withBaseUrl("/docs/rmc-bestfit/user-guide"),
+      doc_location: addBaseUrl("/docs/rmc-bestfit/user-guide"), // Use addBaseUrl
       doc_name: "RMC-BestFit User's Guide",
       active: true,
     },
@@ -47,7 +47,7 @@ export default function Home() {
   const lifeSimData = [
     {
       icon: "img/LifeSim.png",
-      doc_location: withBaseUrl("/docs/lifesim/users-guide"),
+      doc_location: addBaseUrl("/docs/lifesim/users-guide"), // Use addBaseUrl
       doc_name: "LifeSim User's Guide",
       active: true,
     },
@@ -56,67 +56,67 @@ export default function Home() {
   const toolboxSuiteData = [
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/breach-parameters-suite"),
+      doc_location: addBaseUrl("/breach-parameters-suite"), // Use addBaseUrl
       doc_name: "Breach Parameters Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/flood-hazard-suite"),
+      doc_location: addBaseUrl("/flood-hazard-suite"), // Use addBaseUrl
       doc_name: "Flood Hazard Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/internal-erosion-suite"),
+      doc_location: addBaseUrl("/internal-erosion-suite"), // Use addBaseUrl
       doc_name: "Internal Erosion Suite",
       active: true,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/overtopping-erosion-suite"),
+      doc_location: addBaseUrl("/overtopping-erosion-suite"), // Use addBaseUrl
       doc_name: "Overtopping Erosion Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/risk-calculations-suite"),
+      doc_location: addBaseUrl("/risk-calculations-suite"), // Use addBaseUrl
       doc_name: "Risk Calculations Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/riverine-erosion-suite"),
+      doc_location: addBaseUrl("/riverine-erosion-suite"), // Use addBaseUrl
       doc_name: "Riverine Erosion Toolbox",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/hydraulic-fracture-toolbox"),
+      doc_location: addBaseUrl("/hydraulic-fracture-toolbox"), // Use addBaseUrl
       doc_name: "Hydraulic Fracture Toolbox",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/rmc-qracalcs-suite"),
+      doc_location: addBaseUrl("/rmc-qracalcs-suite"), // Use addBaseUrl
       doc_name: "RMC-QRAcalcs",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/seismic-hazard-suite"),
+      doc_location: addBaseUrl("/seismic-hazard-suite"), // Use addBaseUrl
       doc_name: "Seismic Hazard Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/spillway-erosion-suite"),
+      doc_location: addBaseUrl("/spillway-erosion-suite"), // Use addBaseUrl
       doc_name: "Spillway Erosion Suite",
       active: false,
     },
     {
       icon: "img/Toolbox.png",
-      doc_location: withBaseUrl("/structural-suite"),
+      doc_location: addBaseUrl("/structural-suite"), // Use addBaseUrl
       doc_name: "Structural Suite",
       active: false,
     },
