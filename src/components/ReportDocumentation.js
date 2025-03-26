c29861674e7b480411d6f76b619b26d692ebafa5
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-03-26 12:12:55 -0400

    Updates

---
+++ src/components/ReportDocumentation.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const ReportDocumentation({ reportDate, reportType, reportTitle, reportSubTitle, reportAuthors, reportAbstract, reportSubjectTerms, responsiblePersonName, responsiblePersonNumber }) => {
+    return (
+        <div className="report-documentation-table">
+
+        </div>
+    )
+};
+
+export default ReportDocumentation;
