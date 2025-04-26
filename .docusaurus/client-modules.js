30828cc11ebc08450f43b21f376efc2fbeb9834c
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-04-25 20:41:42 -0400

    Removed unnecessary files from git repo after modifying .gitignore to prevent commits of too many files.

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\USACE\\OneDrive\\Documents\\GitHub\\RMC-Software-Documentation\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\USACE\\OneDrive\\Documents\\GitHub\\RMC-Software-Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\USACE\\OneDrive\\Documents\\GitHub\\RMC-Software-Documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\USACE\\OneDrive\\Documents\\GitHub\\RMC-Software-Documentation\\src\\css\\custom.css"),
-];
