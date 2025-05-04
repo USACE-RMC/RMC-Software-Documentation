de40209c1a7ba9a9b4b6bc7463d04fdf7390919f
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-05-04 16:44:10 -0400

    Added chapters to RMC-TotalRisk User's Guide and other updates
    Created 10-consequence-functions.mdx, 11-risk-analyses.mdx, 12-risk-analysis-results.mdx, 13-risk-analysis-diagnostics.mdx, 14-references.mdx, and 15-appendix-acronyms.mdx for the RMC-TotalRisk User's Guide.
    
    Removed the EquationNoRef component prop of "inline" to default to "true" instead of "false".

--- src/components/EquationNoRef.js
+++ src/components/EquationNoRef.js
@@ -4,7 +4,7 @@ import { BlockMath, InlineMath } from "react-katex";
 import "../css/custom.css";
 import "../css/equation.css";
 
-const EquationNoRef = ({ equation, inline = false }) => {
+const EquationNoRef = ({ equation, inline = true }) => {
   const expression = equation;
 
   return (
