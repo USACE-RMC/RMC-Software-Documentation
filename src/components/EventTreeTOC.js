70a02bd1c1cc96fbe8b733f3e64dfcd8d68f5945
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-07-10 18:22:47 -0400

    Updating v1.0 and v1.5 of event tree database

--- src/components/EventTreeTOC.js
+++ src/components/EventTreeTOC.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Link from "@docusaurus/Link";
 import tocData from "@site/src/data/eventTreeToc.json";
+import { useLocation } from "@docusaurus/router";
+import "../css/event-tree-toc.css";
 
-const EventTreeTOC = ({ version }) => {
-  const versionTOC = tocData[version];
+const EventTreeTOC = () => {
+  const location = useLocation();
+  const match = location.pathname.match(/\/(v\d+\.\d+)\//);
+  const version = match ? match[1] : null;
+
+  const versionTOC = version ? tocData[version] : null;
   if (!versionTOC) return <p>No Table of Contents available for version {version}</p>;
 
   return (
     <div>
       {Object.entries(versionTOC).map(([category, links]) => (
-        <details key={category} open>
-          <summary style={{ fontSize: "1.25rem", fontWeight: 600 }}>{category.replace(/-/g, " ").toUpperCase()}</summary>
-          <ul style={{ marginLeft: "1rem" }}>
+        <details key={category}>
+          <summary className="event-tree-toc-summary">{category.replace(/-/g, " ")}</summary>
+          <ul className="event-tree-toc-list">
             {links.map(({ label, path }) => (
               <li key={path}>
                 <Link to={path}>{label}</Link>
