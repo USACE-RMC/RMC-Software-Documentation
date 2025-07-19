82a879a981e57840a70a056b345ad5d9b47a72cb
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-07-19 19:43:01 -0400

    Multiple updates
    Updated Figure and FigureNoRef components to allow the user to define the width of the figure. Adjusted corresponding CSS files accordingly.
    
    Updated Documentation Guide to provide clearer instructions for downloading and installing prerequisite software.

--- src/components/FigureNoRef.js
+++ src/components/FigureNoRef.js
@@ -1,10 +1,15 @@
 import "../css/custom.css";
 import "../css/figure-no-ref.css";
 
-const FigureNoRef = ({ src, alt }) => {
+const FigureNoRef = ({ src, alt, width = "80%" }) => {
   return (
     <figure className="figure-no-ref-container">
-      <img src={`/RMC-Software-Documentation/${src}`} alt={alt} className="figure-no-ref-image" />
+      <img
+        src={`/RMC-Software-Documentation/${src}`}
+        alt={alt}
+        className="figure-no-ref-image"
+        style={{ maxWidth: width }}
+      />
     </figure>
   );
 };
