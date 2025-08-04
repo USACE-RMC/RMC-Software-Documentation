4e11305ad5e0092357dcc4bc4205eff43aa17ba1
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-08-03 22:12:27 -0400

    Migrated NavContainer to Tailwind CSS

--- src/components/archived_components/NavContainer_archived.js
+++ src/components/archived_components/NavContainer_archived.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../css/custom.css";
+import "../css/nav-container.css";
 import NavLink from "./NavLink";
 import VersionSelector from "./VersionSelector";
 
 const NavContainer = ({ link, linkTitle, document }) => {
   return (
-    <div className="flex justify-between items-center p-[10px]">
-      <div className="flex-1">
+    <div className="nav-container">
+      <div className="nav-link-container">
         <NavLink link={link} linkTitle={linkTitle} />
       </div>
-      <div className="flex-none ml-5">
+      <div className="version-container">
         <VersionSelector document={document} />
       </div>
     </div>
