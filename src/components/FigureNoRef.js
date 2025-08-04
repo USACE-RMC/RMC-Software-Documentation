96db729785269266d60a75b4ab45491f978b729f
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-08-03 21:59:41 -0400

    Migrated FigureNoRef to Tailwind CSS

--- src/components/FigureNoRef.js
+++ src/components/FigureNoRef.js
@@ -1,15 +1,9 @@
 import "../css/custom.css";
-import "../css/figure-no-ref.css";
 
 const FigureNoRef = ({ src, alt, width = "80%" }) => {
   return (
-    <figure className="figure-no-ref-container">
-      <img
-        src={`/RMC-Software-Documentation/${src}`}
-        alt={alt}
-        className="figure-no-ref-image"
-        style={{ maxWidth: width }}
-      />
+    <figure className="w-full ml-0 mr-auto my-[1em] py-5 justify-items-start border-y border-border-color">
+      <img src={`/RMC-Software-Documentation/${src}`} alt={alt} className="h-auto block" style={{ maxWidth: width }} />
     </figure>
   );
 };
