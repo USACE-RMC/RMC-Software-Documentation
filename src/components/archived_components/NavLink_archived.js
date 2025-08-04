4d1c12b34b0ca43edbbba2d3de51f0f0aca76dc5
Adam Gohs <adam.c.gohs@usace.army.mil>
2025-08-03 22:10:02 -0400

    Migrated NavLink to Tailwind CSS

--- src/components/archived_components/NavLink_archived.js
+++ src/components/archived_components/NavLink_archived.js
@@ -2,11 +2,12 @@ import React from "react";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import Link from "@docusaurus/Link";
 import "../css/custom.css";
+import "../css/nav-and-print.css";
 
 const NavLink = ({ link, linkTitle }) => {
   return (
-    <div className="flex mb-[var(--ifm-leading)]">
-      <div className="font-usace text-normal cursor-pointer text-ifm-link">
+    <div className="nav-and-print-container">
+      <div className="nav-link">
         <Link to={`${useBaseUrl(link)}`}>&larr; {linkTitle}</Link>
       </div>
     </div>
