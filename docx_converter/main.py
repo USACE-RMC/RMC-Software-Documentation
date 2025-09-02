9f563469fb64fc25864c554197067368dd8ab291
Tiki Gonzalez <tiki.gonzalez21@me.com>
2025-09-02 14:28:26 -0600

    BEP-Initation Work

--- docx_converter/main.py
+++ docx_converter/main.py
@@ -16,7 +16,7 @@ from utils.mdx_writer import write_mdx_files
 environment = "production"  # Change to "production" for production environment
 
 # Assign the file path for figures
-FIGSRC = r"static\figures\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initiation\v1.0"
+FIGSRC = r"figures\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initiation\v1.0"
 
 # Assign the navigation link, title, and document for the NavContainer component
 NAVLINK = r"\toolboxes\internal-erosion-suite"
@@ -27,7 +27,7 @@ NAVDOC = r"toolbox-technical-manuals\internal-erosion-suite\backward-erosion-pip
 if environment == "production":
     DOCX_PATH = r"C:\Technical Documents\RMC-CPD-2023-05 - RMC Backward Erosion Piping (Initiation) Toolbox.docx"
     BIB_PATH = r"C:\Git\RMC-Software-Documentation\static\bibliographies\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initiation\bib.json"
-    FIGURES_DIR = r"C:\Git\RMC-Software-Documentation\static\figures\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initation\v1.0\figures"
+    FIGURES_DIR = r"C:\Git\RMC-Software-Documentation\static\figures\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initation\v1.0"
     MDX_DIR = r"C:\Git\RMC-Software-Documentation\docs\toolbox-technical-manuals\internal-erosion-suite\backward-erosion-piping-initiation\v1.0"
 else:
     # Development paths
