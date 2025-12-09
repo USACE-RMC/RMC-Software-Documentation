cfc56d1408fc9c27fb75a0ee15ec3c361a115cdf
Tiki Gonzalez <tiki.gonzalez21@me.com>
2025-12-09 16:00:11 -0700

    Small fixes in ET DB and FigureNoRef

--- src/components/FigureNoRef.js
+++ src/components/FigureNoRef.js
@@ -3,7 +3,7 @@ import "../css/custom.css";
 const FigureNoRef = ({ src, alt, width = "80%" }) => {
   return (
     <figure className="w-full ml-0 mr-auto my-[1em] py-5 justify-items-start border-y border-border-color">
-      <img src={`/RMC-Software-Documentation/${src}`} alt={alt} className="h-auto block" style={{ maxWidth: width }} />
+      <img src={`/RMC-Software-Documentation/${src}`} alt={alt} className="block h-auto bg-[#f9f9f9] dark:bg-white" style={{ maxWidth: width }} />
     </figure>
   );
 };
